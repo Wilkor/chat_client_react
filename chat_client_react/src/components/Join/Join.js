7b51185354761d0bbd6b0ceedcd331d9d37c1d20
Wilkor <wilkorguitarrista@msn.com>
2020-03-29 17:40:27 +0000

    alterando placeholder

--- chat_client_react/src/components/Join/Join.js
+++ chat_client_react/src/components/Join/Join.js
@@ -10,12 +10,12 @@ export default function SignIn() {
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Chat SafraDocs</h1>
+        <h1 className="heading">Chat - SafraDocs</h1>
         <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+          <input placeholder="Nome" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
         </div>
         <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+          <input placeholder="Sala" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
         </div>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
           <button className={'button mt-20'} type="submit">Entrar</button>
